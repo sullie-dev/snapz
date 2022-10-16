d3b60f5dca4ccb2c76fd910eea4f494f393073f6
Eoghan O'Sullivan <moo.101x2@gmail.com>
2022-10-16 09:00:10 +0000

    Adjust spelling in deployment settings

--- snapz/settings.py
+++ snapz/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [["snapzapp.herokuapp.com", "localhost"]]
+ALLOWED_HOSTS = ["snapzapp.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-     'cloudinary_storage',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'snapzapp',
