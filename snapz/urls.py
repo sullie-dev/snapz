65b2024e465d36a9a1302a09ae9d6472e91d8291
Eoghan O'Sullivan <moo.101x2@gmail.com>
2022-10-24 21:54:27 +0000

    Add authentication through ajango all auth

--- snapz/urls.py
+++ snapz/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from snapzapp.views import home, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('p/<slug:slug>', post, name='post')
+    path('p/<slug:slug>', post, name='post'),
+    path('accounts/', include('allauth.urls'))
+
 ]
