9d9fd22c68c5286e5b1e85bb958e1842c84f9f1b
Eoghan O'Sullivan <moo.101x2@gmail.com>
2022-10-24 18:26:57 +0000

    Set up Post and comment models

--- snapzapp/admin.py
+++ snapzapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('post_id',)}
+    list_filter = ('created_on', )
+    list_display = ('description', 'slug', 'created_on')
+    search_fields = ['description', ]
+    summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'comment_text', 'post', 'created_on')
+    list_filter = ('created_on', 'name')
+    search_fields = ['name', 'comment_text']
--- snapzapp/models.py
+++ snapzapp/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+import random
+import string
+
+
+def idGenerator():
+    upper_string = string.ascii_uppercase
+    lower_string = string.ascii_lowercase
+    numerical = string.digits
+
+    alphabet = upper_string + lower_string + numerical
+    random_character = random.sample(alphabet, 10)
+    slug = "".join(random_character)
+    return slug
 
 # Create your models here.
+
+
+class Post(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+    post_id = models.CharField(max_length=30, default=idGenerator, unique=True )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    image = CloudinaryField('image', default='placeholder')
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.post_id
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    comment_text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.comment_text} - {self.name}"
